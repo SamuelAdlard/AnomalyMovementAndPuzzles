1edf54d4207cca30d40ef48542627b8f04adcf32
Samuel Adlard <sammyadlard@gmail.com>
2022-07-30 18:57:04 +1000

    added item types

---
+++ Assets/Item.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Item : MonoBehaviour
+{
+    public string type = "TEST";
+    
+}
