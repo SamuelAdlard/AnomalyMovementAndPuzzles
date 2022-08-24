e8afc0731ea9e065a67297244270217bcc7cfcf8
Sm0key9 <89950112+Sm0key9@users.noreply.github.com>
2022-08-24 12:34:16 +1000

    Npc talk not working

--- Assets/NPC.cs
+++ Assets/NPC.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    //public override void Interact()
-    //{
-        //Debug.Log("interacting with npc");
-    }
 
-//}
+    public Conversation[] conversations;
+    public string[] playerConversation;
+    int corruptionLevel = 0;
+  
+
+}
+
+public class Conversation
+{
+    public string[] conversation;
+    Conversation(string[] conversation)
+    {
+        this.conversation = conversation;
+    }
+}
--- Assets/WorldInteraction.cs
+++ Assets/WorldInteraction.cs
@@ -2,27 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using TMPro;
 
 public class WorldInteraction : MonoBehaviour
 {
+    public TextMeshProUGUI PressF;
+    
+    bool startedConversation = false;
+    int index = 0;
     void Update()
     {
+        RaycastHit interactionInfo;        
 
-        RaycastHit interactionInfo;
-
-        if (Physics.Raycast(transform.position, transform.forward, out interactionInfo, 100f))
+        
+        if (Physics.Raycast(transform.position, transform.forward, out interactionInfo, 2f)  )
         {
             GameObject interactedObject = interactionInfo.collider.gameObject;
-
-            if(interactedObject.tag == "InteractableObject")
+            if (!startedConversation)
             {
-                //interactedObject.GetComponent<Interactable>().MoveToInteraction(playerAgent);
-                print("hit");
+                if (interactedObject.tag == "InteractableObject")
+                {
+                    PressF.text = "Press F";
+                    if (Input.GetKey(KeyCode.F))
+                    {
+
+                        startedConversation = true;
+
+                    }
+                }
+                else
+                {
+                    PressF.text = "";
+                }
+
+            }
+
+        }
+        else if(!startedConversation)
+        {
+            PressF.text = "";
+        }
+        else
+        {
+            startedConversation = false;
+            index = 0;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.F) && startedConversation)
+        {
+
+
+            if (Talk(index, interactionInfo))
+            {
+                index++;
             }
             else
             {
-                //playerAgent.destination = interactionInfo.point;
+                startedConversation = false;
+                index = 0;
             }
+            
+           
         }
+    }
+
+    bool Talk(int indexForConversation, RaycastHit hit)
+    {
+        //index < hit.transform.GetComponent<NPC>().npcConversation.Length
+        if (true)
+        {
+            //PressF.text = hit.transform.GetComponent<NPC>().npcConversation[index];
+            print(index);
+            return true;
+        }
+        else
+        {
+            print("End");
+            return false;
+        }
+        
     }
 }
